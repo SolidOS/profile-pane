83530a4a8462c561fb583232c66a24aaf7755a87
Angelo Veltens <angelo.veltens@codecentric.de>
2021-03-19 08:34:53 +0100

    install chat-pane for dev mode

--- dev/context.ts
+++ dev/context.ts
@@ -1,19 +1,13 @@
 import {DataBrowserContext, LiveStore, PaneRegistry} from "pane-registry";
 import { store, solidLogicSingleton } from "solid-ui";
+import {longChatPane} from "chat-pane";
 
 export const context: DataBrowserContext = {
   session: {
     store: store as LiveStore,
     paneRegistry: {
       byName: (name: string) => {
-        return {
-          render: () => {
-            return document.createElement('div')
-                .appendChild(
-                    document.createTextNode(`mock ${name} pane`)
-                );
-          }
-        }
+        return longChatPane
       }
     } as PaneRegistry,
     logic: solidLogicSingleton
