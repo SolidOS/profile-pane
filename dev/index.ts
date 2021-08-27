16f5be26eafd8e674e08d5bc26d319b65a495916
Timea Turdean <timea.turdean@gmail.com>
2021-08-27 11:24:11 +0200

    new auth

--- dev/index.ts
+++ dev/index.ts
@@ -1,43 +1,47 @@
-import { sym } from "rdflib";
+import * as $rdf from "rdflib";
 import { default as pane } from "../src";
 import { context, fetcher } from "./context";
-import { authn, widgets } from "solid-ui";
-
-const {
-  currentSession,
-  popupLogin,
-  logout,
-  trackSession,
-} = authn.solidAuthClient;
-
-const loginButton = widgets.button(
-  document,
-  undefined,
-  "Login",
-  async function () {
-    const session = await currentSession();
-    const popupUri = "https://solidcommunity.net/common/popup.html";
-    if (!session) {
-      await popupLogin({ popupUri });
-    }
-  }
-);
-
-const logoutButton = widgets.button(document, undefined, "Logout", () =>
-  logout()
-);
+import * as UI from "solid-ui";
 
 const loginBanner = document.getElementById("loginBanner");
+const webId = document.getElementById("webId");
 
-trackSession((session) => {
-  if (!session) {
-    loginBanner.innerHTML = "";
-    loginBanner.appendChild(loginButton);
-  } else {
-    loginBanner.innerHTML = `Logged in as ${session.webId}`;
-    loginBanner.appendChild(logoutButton);
+const uri = "https://timea-test.solidcommunity.net";
+//loginBanner.appendChild(UI.authn.loginStatusBox(document, null, {}));
+
+if (!UI.authn.authSession.info.isLoggedIn) {
+  // HACK This is a really ugly way to add a login box.
+  // TODO make it prettier
+  //loginBanner.appendChild(UI.authn.logIn(context));
+  loginBanner.appendChild(UI.authn.loginStatusBox(document, uri => {
+    alert("here")
+  }, {}))
+}
+console.dir(UI.authn.authSession)
+console.dir(window.localStorage)
+ UI.authn.authSession.onLogin(() => {
+  if (UI.authn.authSession.info.isLoggedIn) {
+    webId.innerHTML = "Logged in as: " + UI.authn.authSession.info.webId;
+    console.log(UI.authn.authSession.info.webId);
   }
+}); 
+
+/*UI.authn.authSession.onLogin(() => {
+  loginBanner.innerHTML = '';
+  loginBanner.appendChild(UI.authn.loginStatusBox(document, null, {}))
+  // HACK doing this doesn't automatically refresh pages. But, it doesn't work
+  // in the previous version of the data browser, so for now I'm moving on
+  // To test this, navigate to a folder view, then log in. It will not automatically
+  // redirect
+
+})*/
+
+UI.authn.authSession.onSessionRestore(() => {
+  loginBanner.innerHTML = '';
+  loginBanner.appendChild(UI.authn.loginStatusBox(document, null, {}))
+
 });
+
 // https://testingsolidos.solidcommunity.net/profile/card#me
 // https://timbl.inrupt.net/profile/card#me
 //
@@ -46,6 +50,6 @@ const webIdToShow = "https://testingsolidos.solidcommunity.net/profile/card#me";
 // const webIdToShow = "https://timbl.inrupt.net/profile/card#me";
 
 fetcher.load(webIdToShow).then(() => {
-  const app = pane.render(sym(webIdToShow), context);
+  const app = pane.render($rdf.sym(webIdToShow), context);
   document.getElementById("app").replaceWith(app);
 });
