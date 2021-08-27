8bdb7177add978393076c86a996f9580f168738a
Timea Turdean <timea.turdean@gmail.com>
2021-08-27 18:14:26 +0200

    another version of login

--- dev/index.ts
+++ dev/index.ts
@@ -1,46 +1,25 @@
-import * as $rdf from "rdflib";
+import { sym } from "rdflib";
 import { default as pane } from "../src";
 import { context, fetcher } from "./context";
-import * as UI from "solid-ui";
+import { authn } from "solid-ui";
 
-const loginBanner = document.getElementById("loginBanner");
-const webId = document.getElementById("webId");
-
-const uri = "https://timea-test.solidcommunity.net";
-//loginBanner.appendChild(UI.authn.loginStatusBox(document, null, {}));
-
-if (!UI.authn.authSession.info.isLoggedIn) {
-  // HACK This is a really ugly way to add a login box.
-  // TODO make it prettier
-  //loginBanner.appendChild(UI.authn.logIn(context));
-  loginBanner.appendChild(UI.authn.loginStatusBox(document, uri => {
-    alert("here")
-  }, {}))
-}
-console.dir(UI.authn.authSession)
-console.dir(window.localStorage)
- UI.authn.authSession.onLogin(() => {
-  if (UI.authn.authSession.info.isLoggedIn) {
-    webId.innerHTML = "Logged in as: " + UI.authn.authSession.info.webId;
-    console.log(UI.authn.authSession.info.webId);
-  }
-}); 
-
-/*UI.authn.authSession.onLogin(() => {
-  loginBanner.innerHTML = '';
-  loginBanner.appendChild(UI.authn.loginStatusBox(document, null, {}))
-  // HACK doing this doesn't automatically refresh pages. But, it doesn't work
-  // in the previous version of the data browser, so for now I'm moving on
-  // To test this, navigate to a folder view, then log in. It will not automatically
-  // redirect
-
-})*/
-
-UI.authn.authSession.onSessionRestore(() => {
-  loginBanner.innerHTML = '';
-  loginBanner.appendChild(UI.authn.loginStatusBox(document, null, {}))
-
+window.addEventListener("DOMContentLoaded", async () => {
+  alert("whatever");
+  const loginBanner = document.getElementById("loginBanner");
+  rebuildHeader(loginBanner)();
+  authn.authSession.onLogout(rebuildHeader(loginBanner));
+  authn.authSession.onLogin(rebuildHeader(loginBanner));
 });
+
+function rebuildHeader(loginBanner: HTMLElement) {
+  return async () => {
+    const sessionInfo = authn.authSession.info;
+    const user = sessionInfo.webId ? sym(sessionInfo.webId) : null;
+    const webId = document.getElementById("webId");
+    webId.innerHTML = sessionInfo.webId;
+    await loginBanner.appendChild(authn.loginStatusBox(document, null, {}));
+  };
+}
 
 // https://testingsolidos.solidcommunity.net/profile/card#me
 // https://timbl.inrupt.net/profile/card#me
@@ -50,6 +29,6 @@ const webIdToShow = "https://testingsolidos.solidcommunity.net/profile/card#me";
 // const webIdToShow = "https://timbl.inrupt.net/profile/card#me";
 
 fetcher.load(webIdToShow).then(() => {
-  const app = pane.render($rdf.sym(webIdToShow), context);
+  const app = pane.render(sym(webIdToShow), context);
   document.getElementById("app").replaceWith(app);
 });
