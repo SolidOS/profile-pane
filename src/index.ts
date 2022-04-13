9a9ae352022bcc5d3009628d0021d04332ac56a5
Tim Berners-Lee <timbl+github@w3.org>
2022-04-12 23:25:03 -0500

    switch canvas to svg

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { NamedNode, LiveStore } from "rdflib";
 import { render } from "lit-html";
 import { ProfileView } from "./ProfileView";
 import { icons, ns } from "solid-ui";
-import { toCanvas } from 'qrcode'
+import * as qrcode from 'qrcode'
 
 async function loadExtendedProfile(store: LiveStore, subject: NamedNode) {
   const otherProfiles = store.each(
@@ -42,32 +42,35 @@ const Pane = {
     loadExtendedProfile(store, subject).then(() => {
       render(ProfileView(subject, context), target)
 
-      const QRCodeEles = Array.from(context.dom.getElementsByClassName('QRCode'))
+      const QRCodeEles = Array.from(target.getElementsByClassName('QRCode')) // was context.dom
       if (!QRCodeEles.length) return console.error("QRCode Ele missing")
-      for (const canvas of QRCodeEles as HTMLElement[]) {
-        const value = canvas.getAttribute('data-value')
+      for (const QRCodeElement of QRCodeEles as HTMLElement[]) {
+        const value = QRCodeElement.getAttribute('data-value')
         if (!value) return console.error("QRCode data-value missing")
-        const highlightColor = canvas.getAttribute('highlightColor') || '#000000'
-        const backgroundColor = canvas.getAttribute('backgroundColor') || '#ffffff'
-        console.log(`@@ qrcodes2 colours highlightColor ${highlightColor}, backgroundColor ${backgroundColor}`)
+        const highlightColor = QRCodeElement.getAttribute('highlightColor') || '#000000'
+        const backgroundColor = QRCodeElement.getAttribute('backgroundColor') || '#ffffff'
+        // zconsole.log(`@@ qrcodes2 colours highlightColor ${highlightColor}, backgroundColor ${backgroundColor}`)
 
         const options = {
+          type: 'svg',
           color: {
             dark: highlightColor,
             light: backgroundColor
           }
         }
-        // see https://www.npmjs.com/package/qrcode#tocanvascanvaselement-text-options-cberror
-        toCanvas(canvas, value, options, function (error) {
+
+        qrcode.toString(value, options, function (error, svg) {
           if (error) {
             console.error('QRcode error!', error)
           } else {
-            console.log('QRcode success.');
-            canvas.style.width = "80%"
-            canvas.style.height = "80%"
-            canvas.style.margin = "10%"
+            console.log('QRcode success.', svg);
+            QRCodeElement.innerHTML = svg
+            QRCodeElement.style.width = "80%"
+            QRCodeElement.style.height = "80%"
+            QRCodeElement.style.margin = "10%"
           }
         });
+
       }
     })
 
