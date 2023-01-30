380d5df53fed23d3c5e771e63255c7a304b9b4c3
Tim Berners-Lee <timbl+github@w3.org>
2023-01-30 08:55:06 +0000

    work with old or new solid-logic. Fix rdflib import bug

--- src/StuffPresenter.ts
+++ src/StuffPresenter.ts
@@ -47,7 +47,7 @@ export async function presentStuff(
  console.log('scopedItems', scopedItems)
 
  const stuff = scopedItems.map(item => {
-   const icon = getIconForClass(item.type || ns.rdf('Resource'))
+   const icon = getIconForClass((item as any).type || ns.rdf('Resource')) // @@ work with old or new solid-logic
    const href = item.instance.uri
    const name = utils.label(item.instance)
    const instance = item.instance
