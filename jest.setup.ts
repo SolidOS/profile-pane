53bc0123df3747989c146be69c8a7c60fb59a5ee
Tim Berners-Lee <timbl+github@w3.org>
2024-09-11 00:32:40 +0100

    get jest to ignore the .ttl file. tests pass again.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import fetchMock from "jest-fetch-mock";
+import { Buffer } from 'buffer' // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
 const { TextEncoder, TextDecoder } = require('util')
 
 global.TextEncoder = TextEncoder;
