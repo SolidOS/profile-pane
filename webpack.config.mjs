9c0f90cad00c7e6e2adf5d084a84c9b2fa5ac300
Sharon Stratsianis <sstratsianis@gmail.com>
2026-03-10 13:22:50 +1100

    resolved webpack build error

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,11 +1,20 @@
 import path from 'path'
+import { createRequire } from 'module'
 import TerserPlugin from 'terser-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
 
+const require = createRequire(import.meta.url)
+
 const common = {
   entry: './src/index.ts',
+  performance: {
+    hints: false
+  },
   resolve: {
     extensions: ['.ts', '.js', '.json'],
+    fallback: {
+      path: require.resolve('path-browserify')
+    }
   },
   module: {
     rules: [
