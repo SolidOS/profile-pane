54aa6a0494cf782b943a2ebcea6a8d694b667339
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-03-19 15:30:08 +0100

    fix tests

--- test/friend-list.accessibility.test.ts
+++ test/friend-list.accessibility.test.ts
@@ -1,11 +1,12 @@
 import { render } from 'lit-html'
 import * as FriendListModule from '../src/FriendList'
+import * as AddMeToYourFriendsModule from '../src/addMeToYourFriends'
 import axe from 'axe-core'
 
 describe('FriendList accessibility', () => {
   it('has no accessibility violations', async () => {
     // Mock extractFriends to return a static friend list
-    jest.spyOn(FriendListModule, 'extractFriends').mockImplementation(() => {
+    jest.spyOn(AddMeToYourFriendsModule, 'extractFriends').mockImplementation(() => {
       const div = document.createElement('div')
       div.innerHTML = '<li><a href="https://friend.example">Alice</a></li>'
       return div
