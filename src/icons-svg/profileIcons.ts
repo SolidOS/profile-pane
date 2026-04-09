3ebfecba6ffdf06888791c51ae3485bce42da7f5
Sharon Stratsianis <sstratsianis@gmail.com>
2026-04-10 06:38:42 +1000

    icons

---
+++ src/icons-svg/profileIcons.ts
@@ -0,0 +1,145 @@
+import { html } from 'lit-html'
+
+export const birthdayIcon = html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    class="profile__detail-icon"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M13.3327 13.9999V8.66659C13.3327 8.31296 13.1922 7.97382 12.9422 7.72378C12.6921 7.47373 12.353 7.33325 11.9993 7.33325H3.99935C3.64573 7.33325 3.30659 7.47373 3.05654 7.72378C2.80649 7.97382 2.66602 8.31296 2.66602 8.66659V13.9999" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M2.66602 10.6667C2.66602 10.6667 2.99935 10 3.99935 10C4.99935 10 5.66602 11.3333 6.66602 11.3333C7.66602 11.3333 8.33268 10 9.33268 10C10.3327 10 10.9993 11.3333 11.9993 11.3333C12.9993 11.3333 13.3327 10.6667 13.3327 10.6667" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.33398 14H14.6673" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4.66602 5.33325V7.33325" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8 5.33325V7.33325" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.334 5.33325V7.33325" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4.66602 2.66675H4.67268" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8 2.66675H8.00667" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.334 2.66675H11.3407" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`
+export const locationIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+  width="16" 
+  height="16" 
+  viewBox="0 0 16 16" 
+  fill="none"
+  class="profile__detail-icon"
+  aria-hidden="true"
+  focusable="false"
+  >
+    <path d="M13.3327 6.66659C13.3327 9.99525 9.64002 13.4619 8.40002 14.5326C8.2845 14.6194 8.14388 14.6664 7.99935 14.6664C7.85482 14.6664 7.7142 14.6194 7.59868 14.5326C6.35868 13.4619 2.66602 9.99525 2.66602 6.66659C2.66602 5.2521 3.22792 3.89554 4.22811 2.89535C5.22831 1.89516 6.58486 1.33325 7.99935 1.33325C9.41384 1.33325 10.7704 1.89516 11.7706 2.89535C12.7708 3.89554 13.3327 5.2521 13.3327 6.66659Z" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8 8.66675C9.10457 8.66675 10 7.77132 10 6.66675C10 5.56218 9.10457 4.66675 8 4.66675C6.89543 4.66675 6 5.56218 6 6.66675C6 7.77132 6.89543 8.66675 8 8.66675Z" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`
+export const checkIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="14" 
+    height="14" 
+    viewBox="0 0 14 14" 
+    fill="none"
+    class="profile__detail-icon"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M11.254 3.08785C11.4818 2.86004 11.8513 2.86004 12.0791 3.08785C12.3066 3.31559 12.3066 3.68451 12.0791 3.91226L5.66216 10.3292C5.55282 10.4385 5.40458 10.5 5.24996 10.5001C5.09525 10.5001 4.94646 10.4386 4.83707 10.3292L1.92085 7.41226C1.69305 7.18445 1.69305 6.81565 1.92085 6.58785C2.14866 6.36004 2.51746 6.36004 2.74527 6.58785L5.24927 9.09117L11.254 3.08785Z" fill="#7C4DFF"/>
+  </svg>
+`
+export const plusDarkIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="14" 
+    height="14" 
+    viewBox="0 0 14 14" 
+    fill="none"
+    class="profile__detail-icon"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M2.91602 7H11.0827" stroke="#314158" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7 2.9165V11.0832" stroke="#314158" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`
+
+export const plusIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="12" 
+    height="12" 
+    viewBox="0 0 12 12" 
+    fill="none"
+    class="profile__detail-icon"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <g clip-path="url(#clip0_3001_2564)">
+      <path d="M6.06055 0.10083C6.53372 0.101056 6.91793 0.485037 6.91797 0.958252V5.20532H11.165C11.6384 5.20532 12.0225 5.58936 12.0225 6.06274C12.0223 6.536 11.6383 6.92017 11.165 6.92017H6.91797V11.1663C6.91797 11.6395 6.53374 12.0235 6.06055 12.0237C5.58716 12.0237 5.20312 11.6396 5.20312 11.1663V6.92017H0.957031C0.483929 6.91994 0.0997601 6.53586 0.0996094 6.06274C0.0996094 5.5895 0.483837 5.20555 0.957031 5.20532H5.20312V0.958252C5.20316 0.484897 5.58718 0.10083 6.06055 0.10083Z" fill="#7C4DFF"/>
+    </g>
+    <defs>
+      <clipPath id="clip0_3001_2564">
+        <rect width="12" height="12" fill="white"/>
+      </clipPath>
+    </defs>
+  </svg>
+`
+export const searchIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="13" 
+    height="13" 
+    viewBox="0 0 13 13" 
+    fill="none"
+    class="profile__detail-icon"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M6.16457 0C2.76552 0 0 2.76589 0 6.16539C0 9.56509 2.76552 12.3308 6.16457 12.3308C9.56382 12.3308 12.3291 9.56509 12.3291 6.16539C12.3291 2.76589 9.56382 0 6.16457 0ZM6.16457 11.1926C3.39298 11.1926 1.13808 8.93738 1.13808 6.16542C1.13808 3.39346 3.39298 1.13823 6.16457 1.13823C8.93616 1.13823 11.1911 3.39344 11.1911 6.16539C11.1911 8.93735 8.93616 11.1926 6.16457 11.1926Z" fill="#99A1AF"/>
+  </svg>
+`
+export const trashIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="20" 
+    height="20" 
+    viewBox="0 0 20 20" 
+    fill="none"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M8.33301 9.16675V14.1667" stroke="#D1D5DC" stroke-width="1.45833" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.667 9.16675V14.1667" stroke="#D1D5DC" stroke-width="1.45833" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M15.8337 5V16.6667C15.8337 17.1087 15.6581 17.5326 15.3455 17.8452C15.0329 18.1577 14.609 18.3333 14.167 18.3333H5.83366C5.39163 18.3333 4.96771 18.1577 4.65515 17.8452C4.34259 17.5326 4.16699 17.1087 4.16699 16.6667V5" stroke="#D1D5DC" stroke-width="1.45833" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M2.5 5H17.5" stroke="#D1D5DC" stroke-width="1.45833" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.66699 5.00008V3.33341C6.66699 2.89139 6.84259 2.46746 7.15515 2.1549C7.46771 1.84234 7.89163 1.66675 8.33366 1.66675H11.667C12.109 1.66675 12.5329 1.84234 12.8455 2.1549C13.1581 2.46746 13.3337 2.89139 13.3337 3.33341V5.00008" stroke="#D1D5DC" stroke-width="1.45833" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`
+export const bentoIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="18" 
+    height="18" 
+    viewBox="0 0 18 18" 
+    fill="none"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M6.75 9.75C7.16421 9.75 7.5 9.41421 7.5 9C7.5 8.58579 7.16421 8.25 6.75 8.25C6.33579 8.25 6 8.58579 6 9C6 9.41421 6.33579 9.75 6.75 9.75Z" stroke="#99A1AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.75 4.5C7.16421 4.5 7.5 4.16421 7.5 3.75C7.5 3.33579 7.16421 3 6.75 3C6.33579 3 6 3.33579 6 3.75C6 4.16421 6.33579 4.5 6.75 4.5Z" stroke="#99A1AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.75 15C7.16421 15 7.5 14.6642 7.5 14.25C7.5 13.8358 7.16421 13.5 6.75 13.5C6.33579 13.5 6 13.8358 6 14.25C6 14.6642 6.33579 15 6.75 15Z" stroke="#99A1AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.25 9.75C11.6642 9.75 12 9.41421 12 9C12 8.58579 11.6642 8.25 11.25 8.25C10.8358 8.25 10.5 8.58579 10.5 9C10.5 9.41421 10.8358 9.75 11.25 9.75Z" stroke="#99A1AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.25 4.5C11.6642 4.5 12 4.16421 12 3.75C12 3.33579 11.6642 3 11.25 3C10.8358 3 10.5 3.33579 10.5 3.75C10.5 4.16421 10.8358 4.5 11.25 4.5Z" stroke="#99A1AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.25 15C11.6642 15 12 14.6642 12 14.25C12 13.8358 11.6642 13.5 11.25 13.5C10.8358 13.5 10.5 13.8358 10.5 14.25C10.5 14.6642 10.8358 15 11.25 15Z" stroke="#99A1AF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`
+export const starIcon = html`
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    width="30" 
+    height="30" 
+    viewBox="0 0 30 30" 
+    fill="none"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <rect width="30" height="30" rx="5" fill="black" fill-opacity="0.2"/>
+    <path d="M14.1093 6.74536C14.4804 6.01811 15.5196 6.01811 15.8907 6.74536L17.7665 10.4209C17.9118 10.7056 18.1844 10.9037 18.5001 10.9539L22.5754 11.6021C23.3817 11.7303 23.7028 12.7186 23.1259 13.2963L20.2099 16.216C19.984 16.4422 19.8798 16.7627 19.9296 17.0785L20.5726 21.1546C20.6998 21.9611 19.8591 22.5719 19.1314 22.2017L15.4534 20.3307C15.1685 20.1857 14.8315 20.1857 14.5466 20.3307L10.8686 22.2017C10.1409 22.5719 9.30023 21.9611 9.42744 21.1546L10.0704 17.0785C10.1202 16.7627 10.016 16.4422 9.79012 16.216L6.87413 13.2963C6.29716 12.7186 6.61827 11.7303 7.42462 11.6021L11.4999 10.9539C11.8156 10.9037 12.0882 10.7056 12.2335 10.4209L14.1093 6.74536Z" fill="#F4F4F4"/>
+  </svg>
+`
