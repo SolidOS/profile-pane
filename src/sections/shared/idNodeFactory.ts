697cab8d57982dd2ef64dfee58cd77dd8939f838
Sharon Stratsianis <sstratsianis@gmail.com>
2026-04-10 13:33:00 +1000

    use timestamp for id creation

--- src/sections/shared/idNodeFactory.ts
+++ src/sections/shared/idNodeFactory.ts
@@ -1,9 +1,6 @@
 import { NamedNode, sym } from 'rdflib'
 
-const ID_MIN = 1000000000000
-const ID_MAX = 9999999999999
-
 export function createIdNode(doc: NamedNode): NamedNode {
-  const randomId = Math.floor(Math.random() * (ID_MAX - ID_MIN + 1)) + ID_MIN
-  return sym(`${doc.uri}#id${randomId}`) as NamedNode
+  const timestampId = Date.now()
+  return sym(`${doc.uri}#id${timestampId}`) as NamedNode
 }
