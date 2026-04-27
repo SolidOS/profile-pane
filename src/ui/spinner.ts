d61777345919f539fb27009f68e138f8ca52d5e8
Sharon Stratsianis <sstratsianis@gmail.com>
2026-04-27 21:22:45 +1000

    change to saving

--- src/ui/spinner.ts
+++ src/ui/spinner.ts
@@ -3,6 +3,6 @@ import { html, TemplateResult } from "lit-html";
 export function createSpinner (): TemplateResult {
   return html`
     <span class="loading-spinner" aria-hidden="true"></span>
-    <span class="sr-only">Loading...</span>
+    <span class="sr-only">Saving...</span>
   `
 }
