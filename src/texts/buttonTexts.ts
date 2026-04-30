93030e713e4b7b16be3a6fae5eb4c2107c309b2b
Sharon Stratsianis <sstratsianis@gmail.com>
2026-04-30 10:40:34 +1000

    refactor: texts

---
+++ src/texts/buttonTexts.ts
@@ -0,0 +1,4 @@
+export const addMeToYourFriendsButtonText = 'Add as friend'
+export const logInAddMeToYourFriendsButtonText = 'Log in to add as friend'
+export const logInToChatWithMeButtonText = 'Login to chat with me'
+export const friendExistsAlreadyButtonText = 'Already part of friends'
---
+++ src/texts/qrCodeTexts.ts
@@ -0,0 +1,1 @@
+export const scanQrToConnectText = 'Link to Profile'
