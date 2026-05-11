96b06b7d49c24d7b271fd8907ee91768faadcf2c
Sharon Stratsianis <sstratsianis@gmail.com>
2026-05-12 06:52:28 +1000

    fix qrcode styling

--- src/sections/qrcode/QRCodeSection.ts
+++ src/sections/qrcode/QRCodeSection.ts
@@ -2,14 +2,21 @@ import { html } from 'lit-html'
 import { QRCodeCard } from './QRCodeCard'
 import { LiveStore, NamedNode } from 'rdflib'
 import '../../styles/QRCodeSection.css'
+import { scanQrToConnectText } from '../../texts'
 
 export function renderQRCodeSection(subject: NamedNode, store: LiveStore) {
   return html`
-      <section class="profile__section border-lighter profile__qr-code flex-column-center" data-profile-section="qr-code" aria-labelledby="qr-heading" tabindex="-1">
+      <section class="profile__section profile__qr-code" data-profile-section="qr-code" aria-labelledby="qr-heading" tabindex="-1">
         <h2 id="qr-heading" class="sr-only">QR code</h2>
-        <div class="qrcode-card__frame flex-center">
+        <div class="qrcode-card__frame">
           ${QRCodeCard(subject, store)}
         </div>
+        <figcaption 
+          id="qr-code-caption" 
+          class="qrcode-card__caption"
+        >
+          ${scanQrToConnectText}
+        </figcaption>
       </section>
   `
 }
