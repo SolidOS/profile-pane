3fa3ae022c31c931fa415546c71dc6e82be38a2d
Sharon Stratsianis <sstratsianis@gmail.com>
2026-05-20 10:23:15 +1000

    #324 fixpage structure headings

--- src/ProfileView.ts
+++ src/ProfileView.ts
@@ -38,17 +38,15 @@ function renderSidebar(
 ) {
   return html`
     <div
-      aria-labelledby="sidebar-heading"
       class="profile__sidebar"
     >
-      <h2 id="sidebar-heading" class="sr-only">Sidebar</h2>
-      <div class="profile__sidebar-content">
+      <aside class="profile__sidebar-content">
         ${renderSocialSection(store, subject, accounts, viewerMode, onSaved)}
         ${renderSkillsSection(store, subject, skills, viewerMode, onSaved)}
         ${renderLanguageSection(store, subject, languages, viewerMode, onSaved)}
         ${renderContactInfoSection(store, subject, contactInfo, viewerMode, onSaved)}
         ${renderQRCodeSection(subject, store)}
-      </div>
+      </aside>
     </div>
   `
 }
