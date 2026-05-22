2ae695b5c9283262e7b3e1aa62fce56f4343458d
Sharon Stratsianis <sstratsianis@gmail.com>
2026-05-22 12:07:21 +1000

    qrcode to h3

--- src/sections/qrcode/QRCodeSection.ts
+++ src/sections/qrcode/QRCodeSection.ts
@@ -6,7 +6,7 @@ import './QRCodeSection.css'
 export function renderQRCodeSection(subject: NamedNode, store: LiveStore) {
   return html`
       <section class="profile__section profile__qr-code" data-profile-section="qr-code" aria-labelledby="qr-heading" tabindex="-1">
-        <h2 id="qr-heading" class="sr-only">QR code</h2>
+        <h3 id="qr-heading" class="sr-only">QR code</h3>
         ${QRCodeCard(subject, store)}
       </section>
   `
