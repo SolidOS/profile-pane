6581b8bfa3a227c3de7fd2cda4c22d1f50dda413
Sharon Stratsianis <sstratsianis@gmail.com>
2026-06-18 19:34:40 +1000

    fix solid logic mocks for new auth

--- __mocks__/solid-logic.js
+++ __mocks__/solid-logic.js
@@ -1,6 +1,12 @@
 const rdf = require('rdflib')
 const store = rdf.graph()
-store.fetcher = { _fetch: () => Promise.resolve({ ok: true, blob: () => Promise.resolve(new Blob()) }) }
+store.fetcher = {
+  _fetch: () => Promise.resolve({ ok: true, blob: () => Promise.resolve(new Blob()) }),
+  load: () => Promise.resolve(undefined),
+}
+store.updater = {
+  update: () => Promise.resolve(undefined),
+}
 
 const noop = () => {}
 const createAutoMock = () => new Proxy({}, {
@@ -14,9 +20,14 @@ const createAutoMock = () => new Proxy({}, {
 const instance = createAutoMock()
 instance.store = store
 
+function currentUser() {
+  const username = globalThis.$SolidTestEnvironment && globalThis.$SolidTestEnvironment.username
+  return username ? rdf.sym(username) : null
+}
+
 module.exports = {
   authSession: { webId: null, isActive: false, info: { isLoggedIn: false, webId: null }, login: noop, logout: noop, events: { on: noop, off: noop } },
-  authn: { checkUser: () => Promise.resolve(null), currentUser: () => null, login: noop },
+  authn: { checkUser: () => Promise.resolve(currentUser()), currentUser, login: noop },
   store,
   solidLogicSingleton: instance,
   SolidLogic: class { constructor() { this.store = store } },
